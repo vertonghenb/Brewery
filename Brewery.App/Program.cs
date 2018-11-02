d14e0c7d573e2f46b09699ef6ee2a411f02d64aa
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2018-11-02 13:34:13 +0100

    Flesh out the application

--- Brewery.App/Program.cs
+++ Brewery.App/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Brewery.Data;
+using Brewery.Domain;
+using System;
+using System.Linq;
 
 namespace Brewery.App
 {
@@ -6,7 +9,28 @@ namespace Brewery.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Deleting DB
+            // Creating DB
+            // Adding a Brewer
+            using(BreweryDbContext context = new BreweryDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Brewer newBrewer = new Brewer("Moortgat", "Brouwerij Duvel Moortgat biedt je een heerlijke brouwerij-ervaring. Ben je een volledige leek of reikt je kennis toch al iets verder? Je ontdekt ongetwijfeld wat je zoekt.");
+
+                context.Brewers.Add(newBrewer);
+
+                context.SaveChanges();
+            }
+
+            // Query The Brewer
+
+            using (BreweryDbContext context = new BreweryDbContext())
+            {
+                Brewer moortgat = context.Brewers.FirstOrDefault();
+                Console.WriteLine(moortgat.Name);
+            }
         }
     }
 }
