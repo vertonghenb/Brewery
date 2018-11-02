22be8d33380c756bacc120afd92a1a1357362f7c
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2018-11-02 13:12:46 +0100

    Add project files.

---
+++ Brewery.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brewery.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
