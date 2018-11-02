6868edc604c74c1873239a72dbf51859fb0b3b04
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2018-11-02 13:22:33 +0100

    Add Domain

---
+++ Brewery.Domain/Beer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Brewery.Domain
+{
+    public class Beer
+    {
+        #region Properties
+        public int BeerId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public double? AlcoholByVolume { get; private set; }
+        public decimal Price { get; private set; }
+        #endregion
+
+        #region Getters
+        public bool AlcoholKnown => AlcoholByVolume.HasValue;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// EF Core Constructor
+        /// </summary>
+        private Beer() { }
+        public Beer(string name, string description, decimal price, double? alcoholByVolume )
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Cannot be Null or Empty", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Cannot be Null or Empty", nameof(description));
+
+            if(price < decimal.Zero)
+                throw new ArgumentException("Cannot be negative", nameof(description));
+
+            Name = name;
+            Description = description;
+            Price = price;
+            AlcoholByVolume = alcoholByVolume;
+        }
+        #endregion
+    }
+}
---
+++ Brewery.Domain/Brewer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brewery.Domain
+{
+    public class Brewer
+    {
+        #region Properties
+        public int BrewerId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime? DateEstablished { get; private set; }
+        #endregion
+
+        #region Collections
+        public ICollection<Beer> Beers { get; private set; } = new HashSet<Beer>();
+        #endregion
+
+        #region Getters
+        public int NrOfBeers => Beers.Count;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// EF Core Constructor
+        /// </summary>
+        private Brewer()
+        {
+
+        }
+        public Brewer(string name, string description, DateTime? dateEstablished = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Cannot be Null or Empty", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(description))
+                throw new ArgumentException("Cannot be Null or Empty", nameof(description));
+
+            Name = name;
+            Description = description;
+            DateEstablished = dateEstablished;
+        }
+        #endregion
+    }
+}
